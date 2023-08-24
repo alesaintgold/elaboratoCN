fd697e907821e1be088bb4027b9f32bb8d848519
campi <answer@autistici.org>
2023-08-24 17:43:49 +0200

    newton function

--- functions/newton.m
+++ functions/newton.m
@@ -1,7 +1,6 @@
-function l = lagrange(x,y,xq)
+function l = newton(x,y,xq)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-
 n = length(x);
 if n ~= length(y)
     error("il numero di punti sulle ascisse x non è coerente con il numero di quelli sulle ordinate");
@@ -9,13 +8,14 @@ end
 if n ~= length(unique(x))
     error("ad una stessa ascissa non possono corrispondere più punti")
 end
-l = zeros(size(xq));
-for k = 1:n
-    Lkn = ones(size(xq));
-    for j = 1:n
-        if k ~= j
-            Lkn = Lkn .* ((xq - x(j))/(x(k) - x(j)));
-        end
+DD = y;
+for k = 1:n-1
+    for r = n:-1:k+1
+        DD(r) = (DD(r) - DD(r-1)) ./ (x(r) - x(r-k));
     end
-    l = l + y(k)*Lkn;
+end
+l = ones(size(xq)) * DD(n);
+for k = n-1:-1:1
+    l = l .* (xq - x(k)) + DD(k);
+end
 end
