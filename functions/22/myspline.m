04028e7eca473f00063ffbce0c8db27ffb94271d
Ale Santoro <alexi.santoro@gmail.com>
2023-10-12 19:14:51 +0200

    ci sto iniziando a capire qualcosa :)

--- functions/22/myspline.m
+++ functions/22/myspline.m
@@ -11,4 +11,12 @@ function yy = myspline( xi, fi, xx, type )
 %       type:   valore che stabilisce il tipo di spline da creare
 %   Output: 
 %       yy:     valori assunti dalla spline nei rispettivi punti xx 
+if nargin < 3, error("argomenti essenziali assenti"); end
+if nargin == 3, type = 1; end
+if size(xi) ~= size(fi), error("Le quantità di dati forniti per " + ...
+        "l'interpolazione non corrispondono"); end
+if length(xi) ~= length(unique(xi)), error("Le ascisse di " + ...
+        "interpolazione devono essere tutte distinte tra loro");end
+% si utilizzerà la fattorizzazione LU per trovare i membri m0...mn +
+load("../8/mialu.m");
 end
