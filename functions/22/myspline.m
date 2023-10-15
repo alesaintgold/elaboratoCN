f83062966c4e4e1764ed55c7948991d7fb7a5092
Ale Santoro <alexi.santoro@gmail.com>
2023-10-15 16:29:24 +0200

    ho abbastanza chiaro come fare il 22, sto avendo problemi con la teoria :(

--- functions/22/myspline.m
+++ functions/22/myspline.m
@@ -1,4 +1,4 @@
-function yy = myspline( xi, fi, xx, type )
+function yy = myspline( x, fi, xx, type )
 %
 %   Se type è uguale a 0 allora calcola la spline cubica interpolante 
 %   naturale i punti (xi(i),fi(i)), mentre se type è diverso da 0 allora 
@@ -17,6 +17,41 @@ if size(xi) ~= size(fi), error("Le quantità di dati forniti per " + ...
         "l'interpolazione non corrispondono"); end
 if length(xi) ~= length(unique(xi)), error("Le ascisse di " + ...
         "interpolazione devono essere tutte distinte tra loro");end
-% si utilizzerà la fattorizzazione LU per trovare i membri m0...mn +
-load("../8/mialu.m");
+% si utilizzerà la fattorizzazione LU per trovare i membri m0...mn 
+n = length(xi);
+for i = 1:n, h(i) = x(i+1)-x(i);end
+for i = 1:n-1, phi(i) = h(i)/(h(i)+h(i+1));end
+for i = 1:n-1, xi(i) = h(i+1)/(h(i)+h(i+1));end
+if type ==0 % spline naturale 
+    m(1)= 0;
+    M = diag(2*ones(1,n)) + diag(xi(2:n-2)*ones(1,n-1),1) + ...
+        diag(phi(2:n-1)*ones(1,n-1),-1);
+
+    % qui ci vanno le differenze divise 
+    % f[x0,x1,x2] f[x1,x2,x3] ... f[xn-2,xn-1,xn]
+
+    % risolvere con fattorizzazione LU il sistema M*m = 6*f e il 
+    % risultato è m(2:n)
+
+    m(n+1) = 0;
+
+else %spline not-a-knot
+    
+    m(1) = 0;
+
+    M = diag(2*ones(1,n)) + diag(xi(2:n-2)*ones(1,n-1),1) + ...
+        diag(phi(2:n-1)*ones(1,n-1),-1);
+    % la matrice M va un po' modificata (vedere appunti)
+
+    %risolvere il sistema come prima
+    
+    % m0 = 6f[x0,x1,x2]-m1-m2
+    % mn = 6f[x(n-2),x(n-2),xn]-m(n-1)-m(n-2)
+end
+
+%eventualmente tirare fuori dall'if-else parti uguali
+
+%utilizzare gli m e gli h per la formula della spline (vedere 
+% appunti o libro) e poi calcolare
+
 end
