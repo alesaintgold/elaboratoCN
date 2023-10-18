29ba3f6d5d00b0688c64b991803db7cf01df2b31
Ale Santoro <alexi.santoro@gmail.com>
2023-10-18 18:17:49 +0200

    finito 22 :))))))))))))

--- functions/22/myspline.m
+++ functions/22/myspline.m
@@ -19,35 +19,71 @@ if length(xi) ~= length(unique(xi)), error("Le ascisse di " + ...
         "interpolazione devono essere tutte distinte tra loro");end
 
 n = length(xi)-1;
-fprintf("n = " +n +"\n");
 
+h = zeros(1,n);
 for i=1:n, h(i)=xi(i+1)-xi(i);end
+phi = zeros(1,n);
+xhi = zeros(1,n);
 for i=1:n-1
     phi(i) = h(i)/(h(i)+h(i+1));
     xhi(i) = h(i+1)/(h(i)+h(i+1));
 end
-disp(phi)
-disp(xhi)
 
-f = fi; % differenza divise
-for j = 1:2
-    for i = 3:-1:j+1
+f = fi ; %differenze divise
+for j = 1 : 2
+    for i = n+1 : -1 : j +1
         f(i) = (f(i)-f(i-1))/(xi(i)-xi(i-j));
     end
 end
-f = f(1:n-1);
-if type == 0 %spline naturale 
-    m(1) = 0;
-  
-    M = diag(2*ones(1,n-1))+...
-        diag(xhi(1:n-2),1)+...
-        diag(phi(2:n-1),-1);
-    
-    disp(M)
-    disp(f);
+f = f (3: n+1) ; % Calcolo le differenze divise
 
-    m(2:n-2) = mialu(M,6*f);
-    m(n-1)=0;
+%definizione diagonali del sistema tridiagonale per trovare m0...mn
+if type==0 %spline naturale 
+    a = 2*ones(n-1,1);  %size n-1
+    b = xhi(1:n-2);     % n-2
+    c = phi(2:n-1);      % n-2
+    d = 6*f;
+else %spline not-a-knot
+    a = [1 2-phi(1) 2*ones(n-3,1) 2-xhi(n-1) 1];%size 2+n-3+2=n+1
+    b = [0 xhi(i)-phi(i) xhi(2:n-1)];           % 2+n-2=n
+    c = [phi(1:n-2) phi(n-1)-xhi(n-1) 0];       % n-2+2 = n
+    d = 6*[f(1) f f(end)];
 end
-print(m);
+
+%risoluzione sistema tridiagonale
+dim = length(d);    
+m = zeros(dim,1);
+for i = 2:dim
+    w = c(i-1)/a(i-1);
+    a(i) = a(i)-w*b(i-1);
+    d(i) = d(i)-w*d(i-1);
+end
+
+m(end) = d(end)/a(end);
+for i = (dim-1):(-1):1
+    m(i) = (d(i)-b(i)*m(i+1))/a(i);
+end
+
+if type == 0 %spline cubica
+    m = [0 m' 0];
+else % spline not-a-knot
+    m(1) = m(1)-m(2)-m(3);
+    m(n+1) = m(n+1)-m(n)-m(n-1);
+end
+
+yy = zeros(length(xx),1) ;
+for j = 1:length(xx)
+    for i = 2:length(xi)
+        if ((xx(j)>=xi(i-1) && xx(j)<=xi(i)) || xx(j)<xi(1))
+            r = fi(i-1)-h(i-1)^2/6*m(i-1) ;
+            q = (fi(i)-fi(i-1))/h(i-1)-h(i-1)/6*(m(i)-m(i-1)) ;
+            yy (j) =((xx(j)-xi(i-1))^3*m(i)+ ...
+                (xi(i)-xx(j))^3*m(i-1))/(6*h(i-1))+...
+                q*(xx(j)-xi(i -1))+...
+                r;
+            break
+        end
+    end
+end
+return;
 end
