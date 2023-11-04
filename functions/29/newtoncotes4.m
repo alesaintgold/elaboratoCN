8ade57b2f423636b609dd2fb7c0d5b1749edb12c
Ale Santoro <alexi.santoro@gmail.com>
2023-11-04 20:25:14 +0100

    quasi finito 29

--- functions/29/newtoncotes4.m
+++ functions/29/newtoncotes4.m
@@ -1,4 +1,4 @@
-function [If,nval] = newtoncotes4(fun,a,b,tol,fa,fb,fm)
+function [If,nval] = newtoncotes4(fun,a,b,tol,fa,fb,fmed)
 % Implementa la formula composita adattativa di Newton-Cotes di grado 4 
 % per calcolare il valore dell'integrale della funzione f nell'intervallo
 % [a,b]
@@ -24,10 +24,10 @@ f(2) = feval(fun,x1);%f1
 x3 = a+3*(b-a)/4;
 f(4) = feval(fun,x3);%f3
 
+x2 = a+(b-a)/2;
 if nargin==4
     f(1) = feval(fun,a);%f0
 
-    x2 = a+(b-a)/2;
     f(3) = feval(fun,x2);%f2
     
     f(5) = feval(fun,b);%f4
@@ -35,7 +35,7 @@ if nargin==4
     nval = 5;
 else
     f(1) = fa;
-    f(3) = fm;
+    f(3) = fmed;
     f(5) = fb;
 
     nval = 2;
@@ -45,6 +45,14 @@ h = (b-a)/4;
 w = newtonCotesWeights(4);
 If = h*(w*f);
 
+%calcolo errore (che non so ancora come fare    )
 
+if(err>tol)
+    [IfL,nvalL] = newtoncotes4(fun,a,x2,tol/2,f(1),f(3),f(2));
+    [IfR,nvalR] = newtoncotes4(fun,x2,b,tol/2,f(3),f(5),f(4));
+
+    If = IfL+IfR;
+    nval = nval + nvalL+ nvalR;
+end
 return ;
 end
